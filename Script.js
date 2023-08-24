a9ee599492104e5249bf51cbf751c0a1fdfc718a
willian <willianextreme04@gmail.com>
2023-08-24 10:16:04 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+const html = document.documentElement
+
+//if(html.classList.contains('light')) 
+//    html.classList.remove('light')}
+//}
+//else{
+  //  html.classList.add('light')
+//}
+
+html.classList.toggle('light')
+
+//caso queira mudar a img
+//const img = document.querySelector("#profile img")
+//if (html.classList.contains("light")) {
+//img.setAttribute("src", "./assets/*imagem*")
+
+//} else {
+// img.setAttribute("src", ".assets/*imagem normal*")
+//}
+}
